81e0bee045bc88ec95d0b21bdd31819dec5c286b
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:21:41 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+//Aamir Ayaan Quraishi
+//230701230002
+//Aim - Printing out Hello World
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello world";
+}
+
+/*
+OUTPUT:
+Hello world
+*/
